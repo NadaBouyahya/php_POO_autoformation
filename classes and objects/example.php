84131a2c35c44206b3bd937d13c46305bfc4673c
NadaBouyahya <bouyahyanada.solicode@gmail.com>
2023-01-23 22:52:42 +0000

    .

---
+++ classes and objects/example.php
@@ -0,0 +1,28 @@
+<?php
+class Fruit {
+    //properties
+    public $name;
+    public $color;
+
+    //methods
+    function set_name($name){
+        $this->name = $name;
+    }
+    function set_color($color){
+        $this->color = $color;
+    }
+
+    function get_name(){
+        return $this->name;
+    }
+    function get_color(){
+        return $this->color;
+    }
+}
+
+$apple = new Fruit();
+$apple -> set_name('Apple');
+$apple -> set_color('Green');
+
+echo ('this' . $apple->get_name() . 'is' . $apple->get_color());
+?>
